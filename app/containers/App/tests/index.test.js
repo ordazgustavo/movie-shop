62dff69980bacbe3b07c25f090af6c9745ddfc0e
Gustavo Ordaz <ordazsgustavo@gmail.com>
2019-04-21 14:57:36 -0500

    fix: test coverage issues

--- app/containers/App/tests/index.test.js
+++ app/containers/App/tests/index.test.js
@@ -1,14 +1,5 @@
-import React from 'react'
-import ShallowRenderer from 'react-test-renderer/shallow'
-
-import App from '../index'
-
-const renderer = new ShallowRenderer()
-
 describe('<App />', () => {
-  it('should render and match the snapshot', () => {
-    renderer.render(<App />)
-    const renderedOutput = renderer.getRenderOutput()
-    expect(renderedOutput).toMatchSnapshot()
+  it('Expect to have unit tests specified', () => {
+    expect(true).toBe(false)
   })
 })
